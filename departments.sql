22d290fc918c70a7278e34f6439deac572c3a278
madidun <119899172+madidun@users.noreply.github.com>
2023-06-11 22:41:51 -0400

    Add files via upload

---
+++ departments.sql
@@ -0,0 +1,9 @@
+-- Create a departments2 table
+
+CREATE TABLE departments2 (
+  dept_no VARCHAR(5) NOT NULL,
+  dept_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (dept_no)
+);
+
+select * from departments;
